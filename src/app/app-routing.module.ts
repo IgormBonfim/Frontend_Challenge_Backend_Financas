be17bb950ab6ac0fd756ff8c7ee106c3cdfd60b3
Igor Bonfim <igormbonfim@hotmail.com>
2022-08-30 22:43:15 -0300

    Adiciona box na pagina de detalhes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { DetalhesReceitaComponent } from './Components/receitas/detalhes-receita/detalhes-receita.component';
 import { AdicionarReceitaComponent } from './Components/receitas/adicionar-receita/adicionar-receita.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -10,8 +11,9 @@ const routes: Routes = [
   { path: "", pathMatch: "full", redirectTo: "home" },
   { path: "home", component: HomeComponent },
   { path: "cadastrar", component: CadastrarComponent },
-  { path: "receitas", component: ReceitasComponent},
-  { path: "receitas/adicionar", component: AdicionarReceitaComponent}
+  { path: "receitas", component: ReceitasComponent },
+  { path: "receitas/adicionar", component: AdicionarReceitaComponent },
+  { path: "receitas/detalhes", component: DetalhesReceitaComponent }
 ];
 
 @NgModule({
